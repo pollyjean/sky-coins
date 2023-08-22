dcd98ca6d96b53acbb7eab1c7f3c7a6f9de4d522
sky(skysurfr) <skysurfr@me.com>
2023-08-22 17:32:57 +0900

    Form 기본 만들기 + 에러처리

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,21 +2,27 @@ import { useState } from "react";
 
 const TodoList = () => {
   const [todo, setTodo] = useState("");
+  const [todoError, setTodoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
+    setTodoError("");
     setTodo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(todo);
+    if (todo.length < 10) {
+      setTodoError("10글자 이상이어야 합니다.");
+    }
+    console.log("Submitted : ", todo);
   };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <label htmlFor="todo">Write a To-Do</label>
         <input type="text" id="todo" name="todo" value={todo} onChange={onChange} />
+        {todoError === "" ? null : <p>{todoError}</p>}
         <button>Add</button>
       </form>
     </div>
